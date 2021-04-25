8df1f9f306e2d4e8be9476b0fe9ef740571820e7
doanvu2000 <doanvu20122000@gmail.com>
2021-04-26 00:07:07 +0700

    update form login,menu in branch DoanVu

--- BTL_CSharp/BTL_CSharp/Program.cs
+++ BTL_CSharp/BTL_CSharp/Program.cs
@@ -16,7 +16,7 @@ namespace BTL_CSharp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmLogin()); 
 
         }
     }
